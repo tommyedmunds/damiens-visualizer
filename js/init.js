279999c0bb7479854169825d04a47baf943bbb83
tommyedmunds <73081204+tommyedmunds@users.noreply.github.com>
2024-03-02 21:01:50 -0500

    shaders yay

--- js/init.js
+++ js/init.js
@@ -7,32 +7,34 @@ function map(value, a, b, c, d) {
 
 onmessage = function (e) {
   // use when you want actual waveform
-  // let y = map(yCoord * 2, 0, width, 0, rows);
-  const { yCoord, xCoord, columns, rows, next, width, board } = e.data;
+  // let y = map(y * 2, 0, width, 0, rows);
+  const { y, x, columns, rows, next, width, height, board } = e.data;
+  const mappedY = map(y, 0, height, 0, rows);
+  const mappedX = map(x, 0, width, 0, columns);
 
-  const y = map(yCoord * 2, 0, width, 0, rows);
-  const x = map(xCoord, 0, width / 2, 0, columns);
-
+  console.log(y, x, mappedX, mappedY, e.data);
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       // Lining the edges with 0s
       if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1) board[i][j] = 0;
       // Filling the rest reactively
-      else if (yCoord < 525) {
+      else if (y < 525) {
         // interesting form, takes actual shape of waveform
-        if (j > y && j < y + 1 && i > x && i < x + 1) {
-          board[i][j] = 1;
+        if (Math.floor(y) % 17 !== 0) {
+          if (j > mappedY && j < mappedY + 1 && i > mappedX && i < mappedX + 1) {
+            board[i][j] = 1;
+          }
+        } else {
+          if (j === Math.floor(mappedY - 1)) {
+            board[i][j] = 1;
+          }
         }
-
-        // if (j === Math.floor(y - 1)) {
-        //   board[i][j] = 1;
-        // }
       } else {
-        // if (!yCoord && !xCoord) {
+        // if (!y && !mappedx) {
         //   board[i][j] = Math.floor(Math.random(2));
         // }
 
-        if (i > x && i < x + 1) {
+        if (i > mappedX && i < mappedX + 1) {
           board[i][j] = 1;
         }
 
@@ -41,5 +43,5 @@ onmessage = function (e) {
     }
   }
 
-  postMessage({ yCoord, xCoord, columns, rows, next, width, board });
+  postMessage({ next, board });
 };
--- js/main.js
+++ js/main.js
@@ -1,218 +1,153 @@
+let golShader;
 let song;
-function preload() {
-  song = loadSound(
-    'https://tommyedmunds.github.io/damiens-visualizer/assets/damiensregicide.mp3'
-  );
-  // song = loadSound('../assets/uzeless.mp3');
-}
+let prevFrame;
+let waveform;
 
-let w;
-let columns;
-let rows;
-let board;
-let next;
-let x = 0;
-let y = 0;
-let h = 0;
+let prevX = 0;
+let prevY = 0;
 
-let waveform = [];
+const density = 1;
 
-let xSpec = 0;
+const verticalParticles = [];
 
-let amplitude;
+let ready = true;
+let storedMilliseconds = 0;
 
-const livingSquares = [];
+let ready2 = true;
+let storedMilliseconds2 = 0;
 
-const generate = new Worker('js/worker.js');
-const initWorker = new Worker('js/init.js');
-
-function setup() {
-  // Set simulation framerate to 10 to avoid flickering
-  frameRate(120);
-  createCanvas(windowWidth, windowHeight);
-
-  song.loop();
-  song.pause();
-
-  // create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(song);
-
-  amplitude = new p5.Amplitude();
-
-  // Connect the Amplitude analyzer to the song
-  amplitude.setInput(song);
-
-  fft = new p5.FFT();
-
-  button = createButton(`${windowWidth}, ${windowHeight}`);
-
-  button.mousePressed(togglePlaying);
-
-  initButton = createButton('spawn random cells');
-
-  initButton.mousePressed(init);
-
-  fft = new p5.FFT();
-  song.amp(0.2);
-
-  w = 9;
-  // Calculate columns and rows
-  columns = floor(width / w);
-  rows = floor(height / w);
-  // Wacky way to make a 2D array is JS
-  board = new Array(columns);
-  for (let i = 0; i < columns; i++) {
-    board[i] = new Array(rows);
-  }
-  // Going to use multiple 2D arrays and swap them
-  next = new Array(columns);
-  for (i = 0; i < columns; i++) {
-    next[i] = new Array(rows);
-  }
-  init(true);
-}
-
-const xThresh = 0.48;
-const yThresh = 0.8;
+let ready3 = true;
+let storedMilliseconds3 = 0;
 
 let angle = 0;
 
+function preload() {
+  golShader = loadShader('js/gol.vert', 'js/gol.frag');
+  song = loadSound(
+    'https://tommyedmunds.github.io/damiens-visualizer/assets/damiensregicide.mp3'
+  );
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  pixelDensity(density);
+  background(0);
+  stroke(255);
+  prevFrame = createGraphics(width, height);
+  prevFrame.pixelDensity(density);
+  prevFrame.noSmooth();
+
+  shader(golShader);
+  golShader.setUniform('normalRes', [density / width, density / height]);
+
+  fft = new p5.FFT();
+  let button = createButton('play song');
+
+  button.mousePressed(togglePlay);
+}
+
 function draw() {
-  background(255);
+  translate(-width / 2, -height / 2, 0);
   waveform = fft.waveform();
 
+  if (frameCount % 200 === 0) {
+    console.log('clear');
+    clear();
+  }
   if (song.isPlaying()) {
-    for (i = 0; i < waveform.length / 2; i++) {
-      x = map(i, 0, waveform.length / 2, 0, width);
-      y = map(waveform[i], -1, 1, 0, height) + sin(angle) * 20;
-      angle += random(0, 1);
-      // use worker here
-      // initWorker.postMessage({ y, x, columns, rows, next, width, board });
-      // initWorker.onmessage = function (e) {
-      //   board = e.data.board;
-      //   next = e.data.next;
-      // };
+    for (let i = 0; i < waveform.length; i++) {
+      let x = map(i, 0, waveform.length, 0, width);
+      let y = map(waveform[i] / 1.4, -1, 1, 0, height);
 
-      // if (floor(random(2)) % 2 === 0) {
-      // if (frameCount % 2 === 0) {
-      init(y, x);
-      // }
-    }
-    // console.log(y, x);
-    // init(y, x);
-  }
-  // init(y, x);
-  // generate();
+      if (y > 550) {
+        let heightVar = 0;
 
-  generate.postMessage({ columns, rows, board });
-  generate.onmessage = function (e) {
-    next = e.data.next;
-    board = e.data.board;
-  };
-  // worker goes here
+        if (Math.abs(millis() - storedMilliseconds) > 15) {
+          ready = true;
+        }
 
-  for (let i = 0; i < columns; i++) {
-    for (let j = 0; j < rows; j++) {
-      if (board[i][j] == 1) {
-        fill(0); // Set the color based on y
+        if (ready) {
+          console.log('fired', y);
+          // line(width / 2, 0, 100, width / 2, height, 100);
+          strokeWeight(50);
+          line(0, x, 100, width, x, 100);
+          // if (y % 5 === 0) {
+
+          // } else {
+          //   strokeWeight(50);
+          //   line(width / 2, 0, 100, width / 2, height, 100);
+          // }
+
+          ready = false;
+          storedMilliseconds = millis();
+        }
+        // for (let j = 0; j < 50; j++) {
+        //   // point(x, random(0, height), 1);
+        //   point(width / 2, heightVar, 1);
+        //   heightVar += height / 57;
+
+        //   if (heightVar > height) {
+        //     heightVar = random(0, height - 200);
+        //   }
+        // }
+
+        // for (let i = 1; i < height * 0.01; i++) {
+        //   stroke(color(random(0, 255), random(0, 255), random(0, 255)));
+        //   strokeWeight(3);
+        //   point(x * 3, random(0, y) * 2);
+        // }
       } else {
-        fill(255);
-      }
-      noStroke();
-      // rect(i * w, j * w, w - 1, w - 1);
-      rect(i * w, j * w, w, w);
-    }
-  }
-}
-
-function calcWidth(x, rows, columns) {
-  if (!x) x = 0;
-  const spaceLeft = map(x, 0, rows, 0, 0.49);
-  const spaceRight = 1 - spaceLeft;
-  return [columns * spaceLeft, columns * spaceRight];
-}
-
-// Fill board randomly
-function init(yCoord, xCoord, shake = false) {
-  // use when you want actual waveform
-  // let y = map(yCoord * 2, 0, width, 0, rows);
-
-  const y = map(yCoord, 0, height, 0, rows);
-  const x = map(xCoord, 0, width, 0, columns);
-
-  for (let i = 0; i < columns; i++) {
-    for (let j = 0; j < rows; j++) {
-      // Lining the edges with 0s
-      if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1) board[i][j] = 0;
-      // Filling the rest reactively
-      else if (yCoord < 525) {
-        // interesting form, takes actual shape of waveform
-        if (floor(yCoord) % 17 !== 0) {
-          if (j > y && j < y + 1 && i > x && i < x + 1) {
-            board[i][j] = 1;
-          }
-        } else {
-          if (j === floor(y - 1)) {
-            board[i][j] = 1;
-          }
-        }
-      } else {
-        if ((!yCoord && !xCoord) || shake) {
-          board[i][j] = floor(random(2));
+        // for (let j = 0; j < 2; j++) {
+        // point(x, y, 1);
+        // }
+        const sWeight = map(x, 0, width, 100, 300);
+        if (Math.abs(millis() - storedMilliseconds2) > 10) {
+          ready2 = true;
         }
 
-        if (i === floor(x)) {
-          board[i][j] = 1;
+        if (ready2) {
+          // line(width * 0.25, y, 100, width * 0.25, y + 50, 100);
+          strokeWeight(sWeight);
+          line(width * 0.25, y, 100, width * 0.25, y + sWeight, 100);
+          ready2 = false;
+          storedMilliseconds2 = millis();
         }
 
-        next[i][j] = 0;
+        if (Math.abs(millis() - storedMilliseconds3) > 10) {
+          ready3 = true;
+        }
+
+        if (ready3) {
+          strokeWeight(sWeight);
+          line(width * 0.75, y, 100, width * 0.75, y + sWeight, 100);
+
+          ready3 = false;
+          storedMilliseconds3 = millis();
+        }
+
+        // point(width * 0.25, y, 1);
+        // point(width * 0.75, y, 1);
       }
     }
   }
+
+  // Copy the rendered image into our prevFrame image
+  prevFrame.image(get(), 0, 0);
+  // Set the image of the previous frame into our shader
+  golShader.setUniform('tex', prevFrame);
+
+  // Give the shader a surface to draw on
+  rect(-width / 2, -height / 2, width, height);
 }
 
-// The process of creating the new generation
-// function generate() {
-//   // Loop through every spot in our 2D array and check spots neighbors
-//   for (let x = 1; x < columns - 1; x++) {
-//     for (let y = 1; y < rows - 1; y++) {
-//       // Add up all the states in a 3x3 surrounding grid
-//       let neighbors = 0;
-//       for (let i = -1; i <= 1; i++) {
-//         for (let j = -1; j <= 1; j++) {
-//           neighbors += board[x + i][y + j];
-//         }
-//       }
-
-//       // A little trick to subtract the current cell's state since
-//       // we added it in the above loop
-//       neighbors -= board[x][y];
-//       // Rules of Life
-//       if (board[x][y] == 1 && neighbors < 2) next[x][y] = 0; // Loneliness
-//       else if (board[x][y] == 1 && neighbors > 3) next[x][y] = 0; // Overpopulation
-//       else if (board[x][y] == 0 && neighbors == 3) {
-//         next[x][y] = 1;
-//       } // Reproduction
-//       else next[x][y] = board[x][y]; // Stasis
-//     }
-//   }
-
-//   // Swap!
-//   let temp = board;
-//   board = next;
-//   next = temp;
-// }
-
-function togglePlaying() {
-  if (!song.isPlaying()) {
-    song.play();
-    song.setVolume(0.8);
-    button.html('pause');
-  } else {
+function togglePlay() {
+  if (song.isPlaying()) {
     song.pause();
-    button.html('play');
+  } else {
+    song.loop();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
